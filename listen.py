25bd3a476710420139a4f0dc7fc12f6364bd4878
JJJUhlar <jjuhlar@gmail.com>
2023-06-17 22:27:19 +0100

    outline class methods

---
+++ listen.py
@@ -0,0 +1,16 @@
+class Listener:
+
+    def __init__(self, podcast: str):
+        podcast = self.podcast_feed
+
+    def podcast(self):
+        return self.podcast
+    
+    def config(self, provider):
+        pass
+        
+    def validate_feed(self):
+        pass
+
+    def transcribe(self, format):
+        pass
